12a669738e4f5669fc25b53e5e246d56ee759184
Juan Rodriguez <juan_ars19@hotmail.com>
2025-02-15 03:07:00 -0700

    Package Change

---
+++ create-env.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const envProd = `
+export const environment = {
+  production: true,
+  apiUrl: '${process.env.API_URL}',
+  publicationsUrl: '${process.env.PUBLICATIONS_URL}',
+  imagesUrl: '${process.env.IMAGES_URL}'
+};
+`;
+
+fs.writeFileSync('./src/environments/environment.prod.ts', envProd);
+console.log('✅ environment.prod.ts creado correctamente.');
