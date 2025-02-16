7456e9c52244a5bfcdfeca2612589052053477e4
Juan Rodriguez <juan_ars19@hotmail.com>
2025-02-15 18:36:23 -0700

    Logout Added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   firstLetter: string = '';
   page: string = '';
+  isMenuOpen: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private elementRef: ElementRef) {}
 
   ngOnInit() {
     const firstName = localStorage.getItem('firstName');
@@ -32,5 +34,28 @@ export class NavbarComponent {
 
   capitalizeFirstLetter(text: string) {
     return text.charAt(0).toUpperCase() + text.slice(1);
+  }
+
+  toggleMenu(event: Event) {
+    event.stopPropagation();
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+    this.isMenuOpen = false; // Cierra el menú después de cerrar sesión
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  // Detecta clics fuera del menú para cerrarlo
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.closeMenu();
+    }
   }
 }
